58dab7680ffae0cbaf7c875267825091d9271391
PalinRevno <ronin_il@yahoo.com>
2016-08-18 16:33:35 +0200

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+%nuget% pack "rsync.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
