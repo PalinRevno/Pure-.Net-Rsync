c21b8dfc6ed78be08e66bb72e09c99cdb9fd93fc
PalinRevno <ronin_il@yahoo.com>
2016-08-18 17:37:02 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -14,4 +14,10 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Terrarium.Sdk.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net45
+
 %nuget% pack "rsync.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
