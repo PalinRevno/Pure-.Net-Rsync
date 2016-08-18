40ffb5c9f5f4f3a0277ea7335db44b9ef5354901
PalinRevno <ronin_il@yahoo.com>
2016-08-18 17:29:41 +0200

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
